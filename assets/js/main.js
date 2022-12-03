cec266713ce30190a1dd88741fbb193f4fcae0a4
igor-pekovic-fe <igor.pekovic95@gmail.com>
2022-12-03 13:11:38 +0100

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+const ratingCard = document.getElementById("rating-card");
+const thankYouCard = document.getElementById("thankyou-card");
+const submitBtn = document.getElementById("submit-btn");
+const count = document.querySelector(".count");
+const btns = document.querySelectorAll(".rating-card__button");
+const ratingBtnsContainer = document.querySelector(".rating-card__buttons");
+
+count.textContent = 0;
+
+ratingBtnsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".rating-card__button");
+  if (!clicked) return;
+  btns.forEach((t) => t.classList.remove("active-btn"));
+  clicked.classList.add("active-btn");
+  count.textContent = clicked.textContent;
+});
+
+submitBtn.addEventListener("click", function () {
+  ratingCard.classList.add("hidden");
+  thankYouCard.classList.remove("hidden");
+});
